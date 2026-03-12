2994c7f6930917b3e2a59b2f096bdf509d13b645
Daltrob26 <n1dtr@outlook.com>
2026-03-11 19:50:02 -0600

    finished card data parser

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-Download the dataset from 
-https://www.kaggle.com/datasets/patrickgendotti/mtg-all-cards
+# MTG K Means Clustering
+
+scryfallScrapper.py searches through the scryfall bulk data found at https://scryfall.com/docs/api/bulk-data and extracts data on each card into a CSV.
--- scryfallScraper.py
+++ scryfallScraper.py
@@ -1,1 +1,96 @@
 import json
+import csv
+
+#too add any other keywords or phrases we want to seach for
+#just add it here and it'll go through it
+keywords = {
+    "has_deathtouch": "deathtouch",
+    "has_double_strike": "double strike",
+    "has_first_strike": "first strike",
+    "has_flying": "flying",
+    "has_haste": "haste",
+    "has_lifelink": "lifelink",
+    "has_reach": "reach",
+    "has_trample": "trample",
+
+    "has_combat": "combat",
+    "has_counter_target": "counter target",
+    "has_damage": "damage",
+    "has_defender": "defender",
+    "has_discard": "discard",
+    "has_draw": "draw",
+    "has_search_library": "search your library",
+    "has_etb": "enters the battlefield",
+    "has_exile": "exile",
+    "has_flash": "flash",
+    "has_goad": "goad",
+    "has_hexproof": "hexproof",
+    "has_indestructible": "indestructible",
+    "has_menace": "menace",
+    "has_sacrifice": "sacrifice",
+    "has_scry": "scry",
+    "has_token": "token",
+    "has_vigilance": "vigilance"
+}
+
+def rarityToInt(rarity:str):
+    if rarity == "common": return 0
+    if rarity == "uncommon": return 1
+    if rarity == "rare": return 2
+    if rarity == "mythic": return 3
+
+
+with open("default-cards.json", "r") as f:
+    cards = json.load(f)
+
+with open("mtg_features.csv", "w", newline="") as output:
+    writer = csv.writer(output)
+
+    writer.writerow([
+        "name","power","toughness","cmc","rarity",
+        "is_white","is_blue","is_black","is_red","is_green",
+        "is_creature","is_enchantment","is_instant","is_sorcery",
+        "is_artifact","is_land","is_planeswalker",
+        *keywords.keys()
+    ])
+
+    for card in cards: 
+        name = card.get("name")
+
+        #handle cards with non int p/t mainly *
+        power = int(card["power"]) if card.get("power","").isdigit() else 0
+        toughness = int(card["toughness"]) if card.get("toughness","").isdigit() else 0
+
+        cmc = card.get("cmc", 0)
+        rarity = rarityToInt(card.get("rarity"))
+
+# color identity 
+        colors = card.get("colors", [])
+
+        is_white = 1 if "W" in colors else 0
+        is_blue  = 1 if "U" in colors else 0
+        is_black = 1 if "B" in colors else 0
+        is_red   = 1 if "R" in colors else 0
+        is_green = 1 if "G" in colors else 0
+
+
+
+# card type
+
+        type_line = card.get("type_line", "")
+
+        is_creature = 1 if "Creature" in type_line else 0
+        is_enchantment = 1 if "Enchantment" in type_line else 0
+        is_instant = 1 if "Instant" in type_line else 0
+        is_sorcery = 1 if "Sorcery" in type_line else 0
+        is_artifact = 1 if "Artifact" in type_line else 0
+        is_land = 1 if "Land" in type_line else 0
+        is_planeswalker = 1 if "Planeswalker" in type_line else 0
+
+# search text for keywords
+        text = card.get("oracle_text", "").lower()
+        keyword_flags = [
+            1 if word in text else 0
+            for word in keywords.values()
+        ]
+        writer.writerow([name, power, toughness, cmc, rarity,is_white, is_blue, is_black, is_red, is_green,is_creature, is_enchantment, is_instant, is_sorcery,is_artifact, is_land, is_planeswalker,*keyword_flags])
