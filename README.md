e09581528bce753ccc8ce575f3afe966b7c940b2
Logan Nielsen <loganskier@gmail.com>
2026-04-09 19:54:22 -0600

    Finish cuda implementation

--- README.md
+++ README.md
@@ -20,6 +20,13 @@ scryfallScraper.py has a global variable at the top to adjust the number of time
 
 KMeans Implementations have a global variable to change the ammount of times they are ran to compute an average runtime. 
 
+## Run cuda
+
+```bash
+module load gcc cuda
+sbatch batch.sh
+```
+
 ## Viz.py 
 
 viz.py has a lot of dependencies, they're listed inside the pyproject.toml for the poetry enviornment you can use.
--- kmean.cu
+++ kmean.cu
@@ -1,23 +1,17 @@
 #include <cuda_runtime.h>
-#include <device_launch_parameters.h>
-
 #include <fstream>
 #include <iostream>
 #include <random>
-#include <sstream>
 #include <string>
 #include <vector>
 #include <chrono>
 #include <cstring>
 
-// struct to hold card data
-//  namely to seperate the name from the numerical data
 struct Card {
     std::string name;
     std::vector<double> features;
 };
 
-// parses one row of the datasheet,
 std::vector<std::string> parseCSVRow(const std::string &line) {
     std::vector<std::string> result;
     std::string cur;
@@ -67,7 +61,7 @@ std::vector<Card> readCSV(const std::string &filename) {
     return data;
 }
 
-//write out the data, final col is the cluster the card belongs to
+// write out the data, final col is the cluster the card belongs to
 void writeCSVWithCardData(const std::string &filename, const std::vector<Card> &data, const std::vector<int> &labels) {
     std::ofstream out(filename);
     if (!out.is_open()) {
@@ -92,123 +86,230 @@ void assignClusters(
     const double* data,
     const double* centroids,
     int* labels,
-    int n,
-    int k,
+    int n, 
+    int k, 
     int dim
 ) {
+
     int i = blockIdx.x * blockDim.x + threadIdx.x;
     if (i >= n) return;
 
-    double bestDist = 1e18;
-    int bestCluster = 0;
+    double best = 1e18;
+    int bestC = 0;
 
-    for (int c = 0; c < k; ++c) {
-        // Compute distance
-        double sum = 0.0;
-        for (int d = 0; d < dim; ++d) {
+    for (int c = 0; c < k; c++) {
+        double dist = 0.0;
+
+        for (int d = 0; d < dim; d++) {
             double diff = data[i * dim + d] - centroids[c * dim + d];
-            sum += diff * diff;
+            dist += diff * diff;
         }
-        // Technically don't need square root since just do direct comparrisions, so no reason to waste time computing it
 
-        if (sum < bestDist) {
-            bestDist = sum;
-            bestCluster = c;
+        if (dist < best) {
+            best = dist;
+            bestC = c;
         }
     }
 
-    labels[i] = bestCluster;
+    labels[i] = bestC;
 }
 
-std::vector<int> kMeansCUDA(const std::vector<Card>& data, int k, int max_iters) {
+__global__
+void buildPointContributions(
+    const double* data,
+    const int* labels,
+    double* contribSums,
+    int* contribCounts,
+    int n, 
+    int dim
+) {
+
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
+    if (i >= n) return;
+
+    int c = labels[i];
+
+    contribCounts[i] = c;
+
+    for (int d = 0; d < dim; d++) {
+        contribSums[i * dim + d] = data[i * dim + d];
+    }
+}
+
+__global__
+void reduce(
+    const double* contribSums,
+    const int* contribCounts,
+    double* sums,
+    int* counts,
+    int n, 
+    int k, 
+    int dim
+) {
+
+    int c = blockIdx.x;
+    int d = threadIdx.x;
+
+    if (c >= k || d >= dim) return;
+
+    double sum = 0.0;
+    int count = 0;
+
+    for (int i = 0; i < n; i++) {
+
+        if (contribCounts[i] == c) {
+            sum += contribSums[i * dim + d];
+
+            if (d == 0)
+                count++;
+        }
+    }
+
+    sums[c * dim + d] = sum;
+
+    if (d == 0)
+        counts[c] = count;
+}
+
+std::vector<int> kMeansCUDA(
+    const std::vector<Card>& data,                        
+    int k, 
+    int max_iters
+) {
+
     int n = data.size();
     int dim = data[0].features.size();
 
-    // Flatten data to work better with cuda
-    std::vector<double> data_flat(n * dim);
-    for (int i = 0; i < n; ++i) {
-        std::memcpy(
-            data_flat.data() + i * dim,
-            data[i].features.data(),
-            dim * sizeof(double)
-        );
-    }
+    std::vector<double> flat(n * dim);
 
-    std::vector<int> labels(n, 0);
+    for (int i = 0; i < n; i++)
+        std::memcpy(flat.data() + i * dim,
+                    data[i].features.data(),
+                    dim * sizeof(double));
 
-    // seed the random starting cards to be the centroids
-    // 08051993 because thats mtg's birthday
-    std::mt19937 rng(851993);
+    std::vector<int> labels(n);
 
     std::vector<double> centroids(k * dim);
-    std::uniform_int_distribution<int> dist(0, n - 1);
 
-    for (int i = 0; i < k; ++i) {
+    // seed the random starting cards to be the centroids
+    //  08051993 because thats mtg's birthday
+    std::mt19937 rng(851993);
+    std::uniform_int_distribution<int> dist(0, n - 1);
+    for (int c = 0; c < k; c++) {
         int idx = dist(rng);
         // print the names of cards used for centroids
-        // std::cout << data[index].name << "\n";
-        std::memcpy(
-            centroids.data() + i * dim,
-            data[idx].features.data(),
-            dim * sizeof(double)
-        );
+        //  std::cout << data[idx].name << "\n";
+        std::memcpy(&centroids[c * dim],
+                    data[idx].features.data(),
+                    dim * sizeof(double));
     }
 
-    // Device memory
-    double *d_data = nullptr;
-    double *d_centroids = nullptr;
-    int *d_labels = nullptr;
+    // GPU memory
+    double *d_data, *d_centroids;
+    int *d_labels;
+
+    double *d_contribSums;
+    int *d_contribCounts;
+
+    double *d_sums;
+    int *d_counts;
 
     cudaMalloc(&d_data, n * dim * sizeof(double));
     cudaMalloc(&d_centroids, k * dim * sizeof(double));
     cudaMalloc(&d_labels, n * sizeof(int));
 
-    cudaMemcpy(d_data, data_flat.data(), n * dim * sizeof(double), cudaMemcpyHostToDevice);
+    cudaMalloc(&d_contribSums, n * dim * sizeof(double));
+    cudaMalloc(&d_contribCounts, n * sizeof(int));
 
-    dim3 DimBlock(256);
-    dim3 DimGrid((n + DimBlock.x - 1) / DimBlock.x);
+    cudaMalloc(&d_sums, k * dim * sizeof(double));
+    cudaMalloc(&d_counts, k * sizeof(int));
 
-    // Main loop
-    for (int iter = 0; iter < max_iters; ++iter) {
-        cudaMemcpy(d_centroids, centroids.data(), k * dim * sizeof(double), cudaMemcpyHostToDevice);
-        assignClusters<<<DimGrid, DimBlock>>>(
-            d_data,
-            d_centroids,
+    cudaMemcpy(d_data, flat.data(),
+               n * dim * sizeof(double),
+               cudaMemcpyHostToDevice);
+
+    dim3 block(256);
+    dim3 grid((n + block.x - 1) / block.x);
+
+    // main loop
+    for (int it = 0; it < max_iters; it++) {
+        cudaMemcpy(d_centroids, 
+            centroids.data(),
+            k * dim * sizeof(double),
+            cudaMemcpyHostToDevice
+        );
+        cudaDeviceSynchronize(); 
+
+        // assign cards to centroids
+        assignClusters<<<grid, block>>>(
+            d_data, 
+            d_centroids, 
             d_labels,
-            n,
+            n, 
             k,
             dim
         );
+        cudaDeviceSynchronize(); 
 
-        cudaMemcpy(labels.data(), d_labels, n * sizeof(int), cudaMemcpyDeviceToHost);
+        // recompute centroids
+        buildPointContributions<<<grid, block>>>(
+            d_data, 
+            d_labels,
+            d_contribSums, 
+            d_contribCounts,
+            n, dim);
+        cudaDeviceSynchronize(); 
 
-        // Recompute centroids
-        std::vector<double> newCentroids(k * dim, 0.0);
-        std::vector<int> counts(k, 0);
+        cudaMemset(d_sums, 0, k * dim * sizeof(double));
+        cudaMemset(d_counts, 0, k * sizeof(int));
 
-        for (int i = 0; i < n; ++i) {
-            int c = labels[i];
-            counts[c]++;
+        // reduction
+        dim3 clusterGrid(k);
+        dim3 featureBlock(dim);
+        reduce<<<clusterGrid, featureBlock>>>(
+            d_contribSums, 
+            d_contribCounts,
+            d_sums, 
+            d_counts,
+            n, 
+            k, 
+            dim
+        );
+        cudaDeviceSynchronize(); 
 
-            for (int d = 0; d < dim; ++d) {
-                newCentroids[c * dim + d] += data_flat[i * dim + d];
+        // copy back
+        std::vector<double> hostSums(k * dim);
+        std::vector<int> hostCounts(k);
+
+        cudaMemcpy(hostSums.data(), d_sums,
+                   k * dim * sizeof(double),
+                   cudaMemcpyDeviceToHost);
+
+        cudaMemcpy(hostCounts.data(), d_counts,
+                   k * sizeof(int),
+                   cudaMemcpyDeviceToHost);
+
+        for (int c = 0; c < k; c++) {
+            if (hostCounts[c] == 0) 
+                continue;
+            for (int d = 0; d < dim; d++) {
+                centroids[c * dim + d] = hostSums[c * dim + d] / hostCounts[c];
             }
         }
-
-        for (int c = 0; c < k; ++c) {
-            if (counts[c] == 0) continue;
-
-            for (int d = 0; d < dim; ++d) {
-                newCentroids[c * dim + d] /= counts[c];
-            }
-        }
-
-        centroids = newCentroids;
     }
 
+    cudaMemcpy(labels.data(), d_labels,
+               n * sizeof(int),
+               cudaMemcpyDeviceToHost);
+
+    // cleanup
     cudaFree(d_data);
     cudaFree(d_centroids);
     cudaFree(d_labels);
+    cudaFree(d_contribSums);
+    cudaFree(d_contribCounts);
+    cudaFree(d_sums);
+    cudaFree(d_counts);
 
     return labels;
 }
